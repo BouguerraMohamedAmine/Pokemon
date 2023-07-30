43eaa417865d1af5674c208f0e975eef0dc20a0d
BouguerraMohamedAmine <med.bouguerraa@gmail.com>
2023-07-30 18:01:36 +0100

    back-end

---
+++ pokeMongoDB/server/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var morgan = require('morgan');
+var db = require('./db');
+const pokemonRouter = require('./pokemon/pokemonRouter.js');
+
+var app = express();
+
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully!!!!!');
+});
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/../client/dist'));
+app.use('/api/pokemon',pokemonRouter)
+
+
+// TODO: Import the pokemonRouter and assign it to the correct route:
+
+var PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log('Poke-MongoDB RESTful API listening on http://localhost/:' + PORT);
+});
+
---
+++ pokeMongoDB/server/pokemon/Pokemon.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+const db = require('../db/index.js');
+mongoose.Promise = global.Promise;
+
+// TODO: Complete the pokemonSchema below.
+var pokemonSchema = new mongoose.Schema({
+    number: { type: Number, unique: true },
+    name: { type: String, unique: true },
+    types: [{ type: String }],
+    description: { type: String },
+  });
+
+// TODO: Register the pokemonSchema with Mongoose as the 'Pokemon' collection.
+var Pokemon = mongoose.model('Pokemon', pokemonSchema);
+
+module.exports = Pokemon;
